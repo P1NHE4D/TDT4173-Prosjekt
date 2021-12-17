cee385c31eb65b822ddf983c0c35689dc91b50a4
P1NHE4D <aleks.michael.g2@googlemail.com>
2021-12-17 13:33:27 +0100

    clean up repository

--- scoring_functions.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics import mean_squared_log_error
-import numpy as np
-
-def root_mean_squared_log_error(y_true, y_pred):
-    return np.sqrt(mean_squared_log_error(y_true, y_pred))
